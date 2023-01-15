b592a129f995461f2f41fa9a8fac451b042c4948
iskako01 <85966697+iskako01@users.noreply.github.com>
2023-01-15 20:10:03 +0100

    Added: search bar

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,49 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Paper, IconButton } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+
+const SearchBar = () => {
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+
+    if (searchValue) {
+      navigate(`/search/${searchValue}`);
+    }
+
+    setSearchValue("");
+  };
+
+  return (
+    <Paper
+      component="form"
+      onSubmit={(e) => handleSubmit(e)}
+      sx={{
+        borderRadius: 20,
+        border: "1px solid #e3e3e3",
+        pl: 2,
+        boxShadow: "none",
+        mr: { sm: 5 },
+      }}
+    >
+      <input
+        className="search-bar"
+        placeholder="Search..."
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+      />
+      <IconButton
+        type="submit"
+        sx={{ p: "10px", color: "red" }}
+        aria-label="search"
+      >
+        <SearchIcon />
+      </IconButton>
+    </Paper>
+  );
+};
+
+export default SearchBar;
---
+++ src/utils/constants.ts
@@ -0,0 +1,49 @@
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
+import HomeIcon from "@mui/icons-material/Home";
+import CodeIcon from "@mui/icons-material/Code";
+import OndemandVideoIcon from "@mui/icons-material/OndemandVideo";
+import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
+import LiveTvIcon from "@mui/icons-material/LiveTv";
+import SchoolIcon from "@mui/icons-material/School";
+import FaceRetouchingNaturalIcon from "@mui/icons-material/FaceRetouchingNatural";
+import CheckroomIcon from "@mui/icons-material/Checkroom";
+import GraphicEqIcon from "@mui/icons-material/GraphicEq";
+import TheaterComedyIcon from "@mui/icons-material/TheaterComedy";
+import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
+import DeveloperModeIcon from "@mui/icons-material/DeveloperMode";
+import { SvgIconTypeMap } from "@mui/material/SvgIcon/SvgIcon";
+
+export const logo = "https://i.ibb.co/s9Qys2j/logo.png";
+interface CategoryInterface {
+  name: string;
+  icon: (SvgIconTypeMap<{}, "svg"> & { muiName: string }) | string;
+}
+
+export const categories: CategoryInterface[] = [
+  { name: "New", icon: typeof HomeIcon },
+  { name: "JS Mastery", icon: typeof CodeIcon },
+  { name: "Coding", icon: typeof CodeIcon },
+  { name: "ReactJS", icon: typeof CodeIcon },
+  { name: "NextJS", icon: typeof CodeIcon },
+  { name: "Music", icon: typeof MusicNoteIcon },
+  { name: "Education", icon: typeof SchoolIcon },
+  { name: "Podcast", icon: typeof GraphicEqIcon },
+  { name: "Movie", icon: typeof OndemandVideoIcon },
+  { name: "Gaming", icon: typeof SportsEsportsIcon },
+  { name: "Live", icon: typeof LiveTvIcon },
+  { name: "Sport", icon: typeof FitnessCenterIcon },
+  { name: "Fashion", icon: typeof CheckroomIcon },
+  { name: "Beauty", icon: typeof FaceRetouchingNaturalIcon },
+  { name: "Comedy", icon: typeof TheaterComedyIcon },
+  { name: "Gym", icon: typeof FitnessCenterIcon },
+  { name: "Crypto", icon: typeof DeveloperModeIcon },
+];
+
+export const demoThumbnailUrl = "https://i.ibb.co/G2L2Gwp/API-Course.png";
+export const demoChannelUrl = "/channel/UCmXmlB4-HJytD7wek0Uo97A";
+export const demoVideoUrl = "/video/GDa8kZLNhJ4";
+export const demoChannelTitle = "JavaScript Mastery";
+export const demoVideoTitle =
+  "Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI";
+export const demoProfilePicture =
+  "http://dergipark.org.tr/assets/app/images/buddy_sample.png";
